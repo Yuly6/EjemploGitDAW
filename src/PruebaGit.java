b6b64a2ac272f28df719280b6dc09aed41ef09ef
Yuly Caraballo <vanehill@gmail.com>
2024-04-09 19:32:23 +0200

    Se añade funcion multiplicacion y se corrigen los numeros

--- src/PruebaGit.java
+++ src/PruebaGit.java
@@ -4,8 +4,9 @@ public class PruebaGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hola Mundo, soy la clase de prueba Git");
-System.out.println("6 + 7 = " + suma(5,7));
-System.out.println("6 -7 =" + resta(5,7));
+System.out.println("6 * 7 = " + multiplicacion(6,7));
+System.out.println("6 + 7 = " + suma(6,7));
+System.out.println("6 -7 =" + resta(6,7));
 
 }
 	public static int suma (int a, int b) {
@@ -14,5 +15,7 @@ System.out.println("6 -7 =" + resta(5,7));
  public static int resta (int a, int b) {
 	 return a - b;
  }
-
+ public static int multiplicacion (int a, int b) {
+	 return a * b;
+}
 }
