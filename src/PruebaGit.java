f1cd38199ebef4ff0f3567ddfbc3a828944cfd64
Yuly Caraballo <vanehill@gmail.com>
2024-04-09 19:07:15 +0200

    Funcion suma añadida

--- src/PruebaGit.java
+++ src/PruebaGit.java
@@ -5,6 +5,9 @@ public class PruebaGit {
 		// TODO Auto-generated method stub
 System.out.println("Hola Mundo, soy la clase de prueba Git");
 
+public static int suma (int a, int b) {
+	return a+b;
+}
 	}
 
 }
