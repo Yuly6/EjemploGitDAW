239176e7b27494de58ecdc7ce9151697b534f63d
Yuly Caraballo <vanehill@gmail.com>
2024-04-09 19:25:16 +0200

    mensaje de suma y resta añadadida

--- src/PruebaGit.java
+++ src/PruebaGit.java
@@ -4,6 +4,8 @@ public class PruebaGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hola Mundo, soy la clase de prueba Git");
+System.out.println("6 + 7 = " + suma(5,7));
+System.out.println("6 -7 =" + resta(5,7));
 
 }
 	public static int suma (int a, int b) {
